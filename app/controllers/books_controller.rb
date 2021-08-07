c2ec40c2d446eb9adf1c10d409db0c2a1244c29a
kitayama3352 <kitayama3352@gmail.com>
2021-08-07 03:39:04 +0000

    Bookers課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
     # ２. データをデータベースに保存するためのsaveメソッド実行
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to show_path(@book.id)
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :index
@@ -31,11 +31,15 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
     end
-    redirect_to "/books/#{book.id}"
+
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
   get '/' => 'homes#top'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'show'
-  get 'books/:id/edit' => 'books#edit', as: 'edit'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update'
-  delete 'books/:id' => 'books#destroy', as: 'destroy'
+  delete 'books/:id' => 'books#destroy'
 end
